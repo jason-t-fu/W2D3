878a375c205f58e82c5d7439e8ce67ad653b4ac0
jason-t-fu <jasonfu1992@gmail.com>
2019-01-16 15:40:22 -0800

    Initial commit for poker

---
+++ poker/spec/poker_spec.rb
@@ -0,0 +1,31 @@
+require 'card'
+require 'deck'
+require 'hand'
+require 'player'
+require 'game'
+
+
+RSpec.describe Card do
+
+
+end
+
+RSpec.describe Deck do
+
+
+end
+
+RSpec.describe Hand do
+
+
+end
+
+RSpec.describe Player do
+
+
+end
+
+RSpec.describe Game do
+
+
+end
